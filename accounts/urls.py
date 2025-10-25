a8c576ae0de6afa84d9d0d29081573a56adc8a86
elicipher <elmira.nikrioodi96@gmail.com>
2025-10-25 18:45:33 +0330

    User update profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("send-otp-code/" , views.SendOTPCodeAPIView.as_view() ),
     path("verify_otp_code/" , views.VerifyOTPCodeAPIView.as_view() ),
     path("register/" , views.UserRegisterAPIView.as_view() ),
+    path("profile/" , views.UserProfileUpdateAPIView.as_view() ),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
