40f7122724291d6c64dc6a67625d896ce036ef95
elicipher <elmira.nikrioodi96@gmail.com>
2025-10-30 14:33:57 +0330

    added media and url settings

--- config/settings.py
+++ config/settings.py
@@ -164,3 +164,14 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
     # OTHER SETTINGS
 }
+
+import os
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
