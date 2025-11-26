715acf904ffa8618c0e37382f8c58c2e32f7ad4a
elicipher <elmira.nikrioodi96@gmail.com>
2025-11-26 13:02:49 +0330

    added drf_yasg package

--- config/urls.py
+++ config/urls.py
@@ -16,22 +16,45 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+# from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include("accounts.urls")),
-    path('products/', include("products.urls")),
-    path('cart/', include("cart.urls")),
+    path('api/admin/', admin.site.urls),
+    path('api/account/', include("accounts.urls")),
+    path('api/products/', include("products.urls")),
+    path('api/cart/', include("cart.urls")),
 
     # YOUR PATTERNS
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
 ]
 if settings.DEBUG:
